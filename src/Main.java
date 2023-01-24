33dfd1abae6cf49167bc04475573ccc1af68cde0
Gustavo Starace <practicodecocina@gmail.com>
2023-01-24 10:37:31 +0100

    unico commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[] cadena = {"Hoy", "es", "un", "dia", "genial." };
+        for (String caracter : cadena)
+        { String resultado = caracter.concat("");
+        System.out.println(resultado + " ");}
+
+    }
+}
